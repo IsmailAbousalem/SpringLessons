72e5d04032c61ee62dbeea91937b14e8d0ede7a8
Ismail <ia.abousalem@gmail.com>
2024-06-28 09:20:06 -0400

    Added Lombok Dependecny & Plugin. Changed the structure of Teacher and Course classes using Lombok Annotations

--- src/main/java/com/ironhack/spring_lessons/model/Teacher.java
+++ src/main/java/com/ironhack/spring_lessons/model/Teacher.java
@@ -1,10 +1,12 @@
 package com.ironhack.spring_lessons.model;
 
 import jakarta.persistence.*;
-
-import java.util.List;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Entity
+@Data
+@NoArgsConstructor
 public class Teacher {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -14,63 +16,9 @@ public class Teacher {
     @OneToOne
     private Address address;
 
-//    Use only if we want a bidirectional relationship
-//    If used, we must be careful not to create a stackOverflow Error (infinite loop)
-//    @OneToMany(mappedBy = "teacher")
-//    List<Course> courses;
-
-    public Teacher() {
-    }
-
-//    NOT NECESSARY. EMPTY CONSTRUCTOR IS NECESSARY
-    public Teacher(String teacher) {
-        this.teacher = teacher;
-    }
-
     public Teacher(String teacher, Address address) {
         this.teacher = teacher;
         this.address = address;
     }
 
-    //    GETTERS AND SETTERS ARE NECESSARY FOR SPRING TO WORK PROPERLY
-    public Integer getId() {
-        return id;
-    }
-
-    public void setId(Integer id) {
-        this.id = id;
-    }
-
-    public String getTeacher() {
-        return teacher;
-    }
-
-    public void setTeacher(String name) {
-        this.teacher = name;
-    }
-
-    public Address getAddress() {
-        return address;
-    }
-
-    public void setAddress(Address address) {
-        this.address = address;
-    }
-
-//    public List<Course> getCourses() {
-//        return courses;
-//    }
-//
-//    public void setCourses(List<Course> courses) {
-//        this.courses = courses;
-//    }
-
-    @Override
-    public String toString() {
-        return "Teacher{" +
-                "id=" + id +
-                ", teacher='" + teacher + '\'' +
-                ", address=" + address +
-                '}';
-    }
 }
